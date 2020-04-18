f4c1141e83db2524f72307bb128e1591a298ba88
Fatih Kablan <kablanfatih34@gmail.com>
2020-04-18 23:59:43 +0300

    auth package created

---
+++ src/main/java/com/kablanfatih/springbootjwt/auth/UserDetailsService.java
@@ -0,0 +1,36 @@
+package com.kablanfatih.springbootjwt.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    private Map<String, String> users = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        users.put("Fatih", passwordEncoder.encode("123"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if (users.containsKey(username)) {
+            return new User(username, users.get(username), new ArrayList<>());
+        }
+        throw new UsernameNotFoundException(username);
+    }
+}
